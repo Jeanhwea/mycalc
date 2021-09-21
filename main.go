d154bdeeb57c1ec24f7b484c8394b2b6723aaf6b
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-09-21 11:57:31 +0800

    update: fix struct visiable

--- main.go
+++ main.go
@@ -1,30 +1,14 @@
 package main
 
-import "fmt"
-
-type MyNum struct {
-    x int
-    y int
-}
-
-func Add(a, b MyNum) MyNum {
-    return MyNum{a.x + b.x, a.y + b.y}
-}
-
-func Sub(a, b MyNum) MyNum {
-    return MyNum{a.x - b.x, a.y - b.y}
-}
-
-func Mul(a, b MyNum) MyNum {
-    var x = a.x*b.x - a.y*b.y
-    var y = a.x*b.y + a.y*b.x
-    return MyNum{x, y}
-}
+import (
+    "fmt"
+    "mycalc/mynum"
+)
 
 func main() {
-    a, b := MyNum{1, 2}, MyNum{3, 1}
-    c := Add(a, b)
+    a, b := mynum.MyNum{1, 2}, mynum.MyNum{3, 1}
+    c := mynum.Add(a, b)
     fmt.Println(c)
-    d := Mul(a, b)
+    d := mynum.Mul(a, b)
     fmt.Println(d)
 }
