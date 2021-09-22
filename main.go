5ac9a20a70862b48a94e6b6ea91e2c83d954037a
Jinghui Hu <hujinghui@buaa.edu.cn>
2021-09-22 22:21:21 +0800

    indent using tab

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-    "fmt"
-    "mycalc/mynum"
+	"fmt"
+	"mycalc/mynum"
 )
 
 func main() {
-    a, b := mynum.MyNum{1, 2}, mynum.MyNum{3, 1}
-    c := mynum.Add(a, b)
-    fmt.Println(c)
-    d := mynum.Mul(a, b)
-    fmt.Println(d)
+	a, b := mynum.MyNum{1, 2}, mynum.MyNum{3, 1}
+	c := mynum.Add(a, b)
+	fmt.Println(c)
+	d := mynum.Mul(a, b)
+	fmt.Println(d)
 }
--- mynum/mynum.go
+++ mynum/mynum.go
@@ -1,20 +1,20 @@
 package mynum
 
 type MyNum struct {
-    X int
-    Y int
+	X int
+	Y int
 }
 
 func Add(a, b MyNum) MyNum {
-    return MyNum{a.X + b.X, a.Y + b.Y}
+	return MyNum{a.X + b.X, a.Y + b.Y}
 }
 
 func Sub(a, b MyNum) MyNum {
-    return MyNum{a.X - b.X, a.Y - b.Y}
+	return MyNum{a.X - b.X, a.Y - b.Y}
 }
 
 func Mul(a, b MyNum) MyNum {
-    x := a.X*b.X - a.Y*b.Y
-    y := a.X*b.Y + a.Y*b.X
-    return MyNum{x, y}
+	x := a.X*b.X - a.Y*b.Y
+	y := a.X*b.Y + a.Y*b.X
+	return MyNum{x, y}
 }
